67baf7bfc4bd550159858f11e1c9884afded0230
Mauricio <68833219+MuricioSantiago22@users.noreply.github.com>
2024-04-24 15:29:39 -0600

    - Se integra Firebase al proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,5 +61,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // FireBase
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-analytics-ktx")
 }
